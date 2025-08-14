e4a441adb3164780287552ca5dc8754da357a18f
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-08-14 16:31:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-}
-
 # Resource Group
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
