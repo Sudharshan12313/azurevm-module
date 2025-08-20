9e53860e4b1e756ddda8e355767061745e5e09e3
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-08-20 16:37:06 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
-output "private_ip" {
-  value = azurerm_network_interface.nic.private_ip_address
+output "linux_vm_private_ip" {
+  description = "Private IP of the Linux VM"
+  value       = azurerm_network_interface.linux_nic.private_ip_address
+}
+
+output "windows_vm_private_ip" {
+  description = "Private IP of the Windows VM"
+  value       = azurerm_network_interface.windows_nic.private_ip_address
 }
