21833418d1c114908a4657f91dddc0a581f769c8
Sudharshan12313 <167956857+Sudharshan12313@users.noreply.github.com>
2025-08-24 12:45:40 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
+# All VMs accumulated across runs
+output "all_vms" {
+  value = local.all_vms
+}
+
+# Private IPs of current NICs
 output "vm_private_ips" {
   value = {
     for name, nic in azurerm_network_interface.vm_nic :
